4e88c1f33702222d9ab576849a89df84d3ac2672
Polleps <polleps@gmail.com>
2015-03-24 19:29:35 +0100

    Method die comment in de db gaat zetten
    Method toegevoegd die de comment in de db moet zetten

--- src/Apppackage/DatabaseManager.java
+++ src/Apppackage/DatabaseManager.java
@@ -5,40 +5,73 @@
  * and open the template in the editor.
  */
 package Apppackage;
+
+import com.restfb.types.Comment;
+import com.restfb.types.Post;
 import java.sql.*;
+import java.util.List;
+
 /**
  *
  * @author Polle
  */
 public class DatabaseManager {
+
     private Connection con;
     private Statement st;
     private ResultSet res;
-    
-    public boolean verifyUser(String userName, String password){
-        
-        try{
+
+    public boolean verifyUser(String userName, String password) {
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            
+
             con = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/barbawapatest", "barba", "Ruggenmerg");
             st = con.createStatement();
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("Error: " + ex);
         }
-        try{
+        try {
             String quary = "select * from Users";
             res = st.executeQuery(quary);
-            while(res.next()){
-                  if (userName.equals(res.getString("userName"))  && password.equals(res.getString("password")) ){
-                      return true;
-                  }
-                
+            while (res.next()) {
+                if (userName.equals(res.getString("userName")) && password.equals(res.getString("password"))) {
+                    return true;
+                }
+
             }
-            
-        } catch(Exception ex){
+
+        } catch (Exception ex) {
             System.out.println("Error: " + ex);
         }
         return false;
+    }
+
+    public void addCommentsToDB(Post.Comments coms) {
+        try {
+            List<Comment> comment = coms.getData();
+            for (Comment c : comment) {
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+
+                    con = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/barbawapatest", "barba", "Ruggenmerg");
+                    st = con.createStatement();
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex);
+                }
+                try{
+                    String quary = "select commentID from Comments where commentID='" + c.getId() + "'";
+                    res = st.executeQuery(quary);
+                    if (!res.first()){
+                        //De comment moet in de DB
+                    }                    
+                }
+                catch(Exception ex){
+                    System.out.println("Error: " + ex);
+                }
+            }
+        } catch (Exception e) {
+            //System.out.println("This post has no comments");
+        }
     }
 }
